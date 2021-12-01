22f38423340d039595b8fb19c401e8cfae7f31e9
Keerthana A <68195812+keerthi1312@users.noreply.github.com>
2021-12-01 22:02:22 +0530

    Update blind.h

--- 3_Implementation/inc/blind.h
+++ 3_Implementation/inc/blind.h
@@ -1,1 +1,26 @@
+/**
+ * @file blind.h
+ * @author Keerthana.A ()
+ * @brief Project to Blind an LED connected to AVR328 MCU GPIO Pin
+ * @version 0.1
+ * @date 2021-04-21
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 
+/**
+ * Macro Definitions
+ */
+
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+#include<LiquidCrystal.h>
+
+LiquidCrystal lcd(13,12,11,10,9,8);
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+
+#define trig 6
+#define echo 7
+#define y A1
