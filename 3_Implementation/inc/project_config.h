817d0c9f9c1bb6310630447eccfdae533ff69fd5
Keerthana A <68195812+keerthi1312@users.noreply.github.com>
2021-12-01 22:07:15 +0530

    Update project_config.h

--- 3_Implementation/inc/project_config.h
+++ 3_Implementation/inc/project_config.h
@@ -1,1 +1,24 @@
+#ifndef __PROJECT_CONFIG_H__
+#define __PROJECT_CONFIG_H__
+/**
+ * @file project_config.h
+ * @author Keerthana.A ()
+ * @brief Configuration file to define pins and ports for the interfaced peripherals
+ * @version 0.1
+ * @date 2021-12-01
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 
+/**
+ * Macro Definitions
+ */
+
+#define trig 6
+#define echo 7
+#define y A1
+
+#include <avr/io.h>
+
+#endif /* __PROJECT_CONFIG_H__ */
